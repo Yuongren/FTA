433878807d29ce529624229f3128584498022648
gitYoungren <youngrengitonga@gmail.com>
2025-02-02 22:04:20 +0300

    code added and changed in fitness-tracker/App.js and index.css

--- fitness-tracker/src/App.js
+++ fitness-tracker/src/App.js
@@ -1,22 +1,22 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-
-// Correct imports, assuming the files are in the src directory
-import RegisterForm from './RegisterForm'; // Make sure the path is correct
-import LoginForm from './LoginForm'; // Make sure the path is correct
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import RegisterForm from './RegisterForm';
+import LoginForm from './LoginForm';
 
 const App = () => {
   return (
     <Router>
-      <div>
+      <div className="container">
+        {/* Navigation Bar */}
         <nav>
-          <a href="/register">Register</a>
-          <a href="/login">Login</a>
+          <Link to="/register">Register</Link>
+          <Link to="/login">Login</Link>
         </nav>
-        <Routes>
-        <Route path="/register" element={<RegisterForm />} />
-        <Route path="/login" element={<LoginForm />} />
 
+        {/* Routes */}
+        <Routes>
+          <Route path="/register" element={<RegisterForm />} />
+          <Route path="/login" element={<LoginForm />} />
         </Routes>
       </div>
     </Router>
